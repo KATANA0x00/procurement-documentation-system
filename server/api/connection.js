bef8fd096f058043c6c16c752f90557e2ef9b765
Thirasit Thothong <94747321+KATANA0x00@users.noreply.github.com>
2025-09-19 14:30:07 +0700

    Update connection.js

--- server/api/connection.js
+++ server/api/connection.js
@@ -1,67 +1,79 @@
-import LdapAuth from "ldapauth-fork";
+import LdapAuth from 'ldapauth-fork'
 import { Client } from 'pg'
 
 const config = useRuntimeConfig()
 
-export function connectLDAP() {
-    console.log('NUXT_LDAP_URL', config.NUXT_LDAP_URL)
-    console.log('NUXT_LDAP_DC', config.NUXT_LDAP_DC)
-    if (config.NUXT_LDAP_URL === '' || config.NUXT_LDAP_DC === '' ) {
-        throw new Error('LDAP configuration is missing');
-    }
+export function connectLDAP () {
+  const ldapUrl = process.env.NUXT_LDAP_URL
+  const ldapDC = process.env.NUXT_LDAP_DC
 
-    const Options = {
-        url: config.NUXT_LDAP_URL,
-        searchBase: config.NUXT_LDAP_DC,
-        searchFilter: '(mail={{username}})',
-        reconnect: false,
-        timeout: 5000,
-        connectTimeout: 3000,
-    }
-    const ldapAuth = new LdapAuth(Options);
+  console.log('NUXT_LDAP_URL', ldapUrl)
+  console.log('NUXT_LDAP_DC', ldapDC)
 
-    return ldapAuth;
+  if (!ldapUrl || !ldapDC) {
+    throw new Error('LDAP configuration is missing')
+  }
+
+  const options = {
+    url: ldapUrl,
+    searchBase: ldapDC,
+    searchFilter: '(mail={{username}})',
+    reconnect: false,
+    timeout: 5000,
+    connectTimeout: 3000
+  }
+
+  return new LdapAuth(options)
 }
 
-export function connectPG() {
-    console.log('NUXT_PG_USER', config.NUXT_PG_USER)
-    console.log('NUXT_PG_HOST', config.NUXT_PG_HOST)
-    console.log('NUXT_PG_BASE', config.NUXT_PG_BASE)
-    console.log('NUXT_PG_PASS', config.NUXT_PG_PASS)
-    if (config.NUXT_PG_USER === '' || config.NUXT_PG_HOST === '' || config.NUXT_PG_BASE === '' || config.NUXT_PG_PASS === '') {
-        throw new Error('PostgreSQL configuration is missing');
-    }
+export function connectPG () {
+  console.log('NUXT_PG_USER', config.NUXT_PG_USER)
+  console.log('NUXT_PG_HOST', config.NUXT_PG_HOST)
+  console.log('NUXT_PG_BASE', config.NUXT_PG_BASE)
+  console.log('NUXT_PG_PASS', config.NUXT_PG_PASS)
+  if (
+    config.NUXT_PG_USER === '' ||
+    config.NUXT_PG_HOST === '' ||
+    config.NUXT_PG_BASE === '' ||
+    config.NUXT_PG_PASS === ''
+  ) {
+    throw new Error('PostgreSQL configuration is missing')
+  }
 
-    const client = new Client({
-        user: config.NUXT_PG_USER,
-        host: config.NUXT_PG_HOST,
-        database: config.NUXT_PG_BASE,
-        password: config.NUXT_PG_PASS,
-        port: config.NUXT_NUXT_PG_PORT,
+  const client = new Client({
+    user: config.NUXT_PG_USER,
+    host: config.NUXT_PG_HOST,
+    database: config.NUXT_PG_BASE,
+    password: config.NUXT_PG_PASS,
+    port: config.NUXT_NUXT_PG_PORT
+  })
+
+  return client
+}
+
+export default defineEventHandler(async event => {
+  try {
+    const ldapAuth = connectLDAP()
+    await ldapAuth.close()
+  } catch (error) {
+    console.error('Error connecting to LDAP:', error)
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Internal Server Error'
     })
+  }
 
-    return client;
-}
+  try {
+    const client = connectPG()
+    client.connect()
+    client.end()
+  } catch (error) {
+    console.error('Error connecting to Database:', error)
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Internal Server Error'
+    })
+  }
 
-export default defineEventHandler(async (event) => {
-    try {
-        const ldapAuth = connectLDAP();
-        await ldapAuth.close();
-    }
-    catch (error) {
-        console.error('Error connecting to LDAP:', error);
-        throw createError({ statusCode: 500, statusMessage: 'Internal Server Error' });
-    }
-
-    try {
-        const client = connectPG();
-        client.connect();
-        client.end();
-    }
-    catch (error) {
-        console.error('Error connecting to Database:', error);
-        throw createError({ statusCode: 500, statusMessage: 'Internal Server Error' });
-    }
-
-    return true;
+  return true
 })
