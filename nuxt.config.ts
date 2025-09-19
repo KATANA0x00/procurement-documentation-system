2ffe653455fae3cb1c7b7457d1ca96a8a957e070
Thirasit Thothong <94747321+KATANA0x00@users.noreply.github.com>
2025-09-19 14:16:15 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,15 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/icon"],
   runtimeConfig: {
-    CRYPTO_SECRET_KEY: process.env.CRYPTO_SECRET,
+    CRYPTO_SECRET_KEY: process.env.CRYPTO_SECRET || '',
 
-    NUXT_LDAP_URL: process.env.NUXT_LDAP_URL,
-    NUXT_LDAP_DC: process.env.NUXT_LDAP_DC,
+    NUXT_LDAP_URL: process.env.NUXT_LDAP_URL || '',
+    NUXT_LDAP_DC: process.env.NUXT_LDAP_DC || '',
 
-    NUXT_PG_USER: process.env.NUXT_PG_USER,
-    NUXT_PG_PASS: process.env.NUXT_PG_PASS,
-    NUXT_PG_HOST: process.env.NUXT_PG_HOST,
-    NUXT_PG_BASE: process.env.NUXT_PG_BASE,
+    NUXT_PG_USER: process.env.NUXT_PG_USER || '',
+    NUXT_PG_HOST: process.env.NUXT_PG_HOST || '',
+    NUXT_PG_BASE: process.env.NUXT_PG_BASE || '',
+    NUXT_PG_PASS: process.env.NUXT_PG_PASS || '',
     NUXT_NUXT_PG_PORT: Number(process.env.NUXT_NUXT_PG_PORT) || 5432,
 
     ON_PRODUCTION: process.env.ON_PRODUCTION,
