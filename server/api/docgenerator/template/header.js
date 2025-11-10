bb10ea4042dd8e85c89826599e2ae7f478cc90da
Thirasit Thothong <94747321+KATANA0x00@users.noreply.github.com>
2025-11-10 14:16:33 +0700

    Update header.js

--- server/api/docgenerator/template/header.js
+++ server/api/docgenerator/template/header.js
@@ -66,7 +66,7 @@ export function header (data, idselect, Header, docmark = '') {
                 data.doc_requester +
                 ' สังกัด ' +
                 data.main_department + 
-                (idselect == 'pj1' ? (' ขอรายงานเสนอเพื่อขอ' + data.doc_type + 'วัสดุการศึกษาให้แก่ ' + ( data.department_id === data.main_department_id ? "ภาควิชา":"หลักสูตร") + data.department) : '') + ' เหตุผลความจำเป็นใช้ในงาน ' +
+                (idselect == 'pj1' ? (' ขอรายงานเสนอเพื่อขอ' + data.doc_type + 'วัสดุการศึกษาให้แก่ ' + ( data.department_id === data.main_department_id ? "":"หลักสูตร") + data.department) : '') + ' เหตุผลความจำเป็นใช้ในงาน ' +
                 data.doc_reason +
                 ' ขอให้จัดซื้อวัสดุการศึกษา ดังรายการต่อไปนี้',
               colSpan: 4,
