6ab0db9ee9ed40ad8b785de2476ce20589551c07
Thirasit Thothong <94747321+KATANA0x00@users.noreply.github.com>
2026-04-03 15:58:08 +0700

    update PM4

--- server/api/docgenerator/template/PM4.js
+++ server/api/docgenerator/template/PM4.js
@@ -1,6 +1,10 @@
 import { tablePayment } from "./tablePayment";
 
 export function docDefinition_PM4(data, refund_person = null) {
+    const total = data.pm_list.reduce(
+        (acc, item) => acc + item.amount_b + item.amount_s * 0.01,
+        0,
+    );
     return [
         {
             text: "แบบฟอร์มคืนเงินยืม",
@@ -8,33 +12,37 @@ export function docDefinition_PM4(data, refund_person = null) {
             alignment: "center",
             fontSize: 14,
         },
-        { text: " " },
         {
-            text: [
-                "ชื่อ  " +
-                    (refund_person === null
-                        ? data.doc_requester
-                        : refund_person) +
-                    "  ภาควิชา  " +
-                    data.department,
-            ],
+            text:
+                "คืนเงินยืม " +
+                (refund_person === null ? data.doc_requester : refund_person) +
+                " ลงวันที่ ................................................... ยอดเงินยืม " +
+                total +
+                " บาท",
             alignment: "center",
         },
-        { text: " " },
+        {
+            text: "ชื่อผู้ยืมต่อ ................................. ลงวันที่ ................................................... ยอดเงินยืม ............... บาท",
+            alignment: "center",
+        },
+        {
+            text: "งบ........................................................สาขาวิชา/ส่วนบริหารงานทั่วไป.........................................",
+            alignment: "center",
+        },
         ...tablePayment(data.pm_list, 24),
         { text: " " },
         {
-            text: [
-                "รวมเป็นเงิน",
-                {
-                    text: " ".repeat(50),
-                    decoration: "underline",
-                    decorationStyle: "dotted",
-                },
-                "บาท",
-            ],
-            alignment: "right",
-            margin: [0, 0, 30, 0],
+            text: "คืนเงินยืม  ................................. ลงวันที่ ................................................... ยอดเงินยืม ............... บาท",
+            marginLeft: 20,
+        },
+        {
+            text: "ชื่อผู้ยืมต่อ ................................. ลงวันที่ ................................................... ยอดเงินยืม ............... บาท",
+
+            marginLeft: 20,
+        },
+        {
+            text: "งบ........................................................สาขาวิชา/ส่วนบริหารงานทั่วไป.........................................",
+            marginLeft: 20,
         },
         { text: " " },
         {
@@ -43,17 +51,10 @@ export function docDefinition_PM4(data, refund_person = null) {
                 widths: ["*", "*", "*", "*", 30],
                 body: [
                     [
+                        {},
+                        {},
                         {
                             text: "เจ้าหน้าที่พัสดุผู้รับ",
-                            alignment: "right",
-                        },
-                        {
-                            text: "." + " ".repeat(40) + ".",
-                            decoration: "underline",
-                            decorationStyle: "dotted",
-                        },
-                        {
-                            text: "เจ้าหน้าที่การเงินผู้รับ",
                             alignment: "right",
                         },
                         {
@@ -64,15 +65,8 @@ export function docDefinition_PM4(data, refund_person = null) {
                         {},
                     ],
                     [
-                        {
-                            text: "วันที่",
-                            alignment: "right",
-                        },
-                        {
-                            text: "." + " ".repeat(40) + ".",
-                            decoration: "underline",
-                            decorationStyle: "dotted",
-                        },
+                        {},
+                        {},
                         {
                             text: "วันที่",
                             alignment: "right",
